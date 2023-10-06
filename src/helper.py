37298ec81025f59116e9580c385f006e444dd9de
Kristof Van Coillie <kristof.vancoillie@collibra.com>
2023-10-06 15:55:38 +0200

    first commit

---
+++ src/helper.py
@@ -0,0 +1,79 @@
+import json
+import uuid
+from typing import List, Optional, Sequence, Union
+
+from pydantic.json import pydantic_encoder
+
+from .models import *
+
+__all__ = ["generate_json_files", "generate_source_code"]
+
+
+def generate_json_files(
+    assets: Sequence[Union[NodeAsset, ParentAsset, LeafAsset]],
+    lineages: List[Lineage],
+    asset_types: List[AssetType],
+    custom_lineage_config: CustomLineageConfig,
+) -> None:
+    """
+    Helper function that generates the json files which can be used as input for custom technical lineage batch format
+
+    :param assets: List of assets which will be used to construct assets.json file
+    :type assets: Sequence[Union[NodeAsset, ParentAsset, LeafAsset]]
+    :param lineages: List of lineage relationships which will be used to construct lineage.json
+    :type lineages: List[Lineage]
+    :param asset_types: List of asset types which will be used to construct metadata.json file
+    :type asset_types: List[AssetType]
+    :param custom_lineage_config: Configuration object
+    :type custom_lineage_config: CustomLineageConfig
+    :returns: nothing
+    :rtype: None
+    """
+    # creating assets.json
+    if assets:
+        with open(custom_lineage_config.output_directory_path / "assets.json", "w") as out_file:
+            json.dump(assets, out_file, default=pydantic_encoder)
+
+    # creating lineage.json
+    with open(custom_lineage_config.output_directory_path / "lineage.json", "w") as out_file:
+        json.dump(lineages, out_file, default=pydantic_encoder)
+
+    # creating metadata.json
+    with open(custom_lineage_config.output_directory_path / "metadata.json", "w") as out_file:
+        metadata = {
+            "version": 3,
+            "application_name": custom_lineage_config.application_name,
+            "asset_types": {asset_type.name: {"uuid": asset_type.uuid} for asset_type in asset_types},
+        }
+        json.dump(metadata, out_file)
+
+
+def generate_source_code(
+    source_code_text: str,
+    custom_lineage_config: CustomLineageConfig,
+    highlights: Optional[List[SourceCodeHighLight]] = None,
+    transformation_display_name: Optional[str] = None,
+) -> SourceCode:
+    """
+    Helper function that generates `SourceCode` object.
+
+    :param source_code_text: Text to use as source code
+    :type source_code_text: str
+    :param custom_lineage_config: Configuration object
+    :type custom_lineage_config: CustomLineageConfig
+    :param highlights: List of SourceCodeHighLight objects
+    :type highlights: List[SourceCodeHighLight], optional
+    :param transformation_display_name: Text to use as transformation name
+    :type transformation_display_name: str
+    :returns: SourceCode object constructed using the provided input
+    :rtype: SourceCode
+    """
+    # generate file name and create directory
+    file_name = f"{str(uuid.uuid4())}.txt"
+    with open(custom_lineage_config.source_code_directory_path / file_name, "w") as out_file:
+        out_file.write(source_code_text)
+    return SourceCode(
+        path=f"{custom_lineage_config.source_code_directory_name}/{file_name}",
+        highlights=highlights,
+        transformation_display_name=transformation_display_name,
+    )
---
+++ src/models.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+from typing import List, Optional, Union
+
+from pydantic import BaseModel, model_validator
+
+__all__ = [
+    "Asset",
+    "AssetType",
+    "AssetProperties",
+    "SourceCode",
+    "SourceCodeHighLight",
+    "NodeAsset",
+    "ParentAsset",
+    "LeafAsset",
+    "Lineage",
+    "CustomLineageConfig",
+]
+
+
+class AssetType(BaseModel):
+    name: str
+    uuid: str
+
+
+class Asset(BaseModel):
+    name: str
+    type: str
+
+
+class AssetProperties(BaseModel):
+    fullname: str
+    domain_id: str
+
+
+class SourceCodeHighLight(BaseModel):
+    start: int
+    len: int
+
+
+class SourceCode(BaseModel):
+    path: str
+    highlights: Optional[List[SourceCodeHighLight]] = None
+    transformation_display_name: Optional[str] = None
+
+
+class NodeAsset(BaseModel):
+    nodes: List[Asset]
+    props: Optional[AssetProperties] = None
+
+
+class ParentAsset(BaseModel):
+    nodes: List[Asset]
+    parent: Asset
+    props: Optional[AssetProperties] = None
+
+
+class LeafAsset(BaseModel):
+    nodes: List[Asset]
+    parent: Asset
+    leaf: Asset
+    props: Optional[AssetProperties] = None
+
+
+class Lineage(BaseModel):
+    src: Union[LeafAsset, ParentAsset]
+    trg: Union[LeafAsset, ParentAsset]
+    source_code: Optional[SourceCode] = None
+
+    @model_validator(mode="after")
+    def verify_allowed_relationship(self) -> "Lineage":
+        if isinstance(self.src, ParentAsset) and isinstance(self.trg, LeafAsset):
+            raise ValueError("If src is a ParentAsset, trg has to be a ParentAsset as well (table level lineage).")
+        return self
+
+
+class CustomLineageConfig:
+    def __init__(self, application_name: str, output_directory: str, source_code_directory_name: str = "source_codes"):
+        self.application_name = application_name
+        self.output_directory_path = Path(output_directory)
+        self.source_code_directory_name = source_code_directory_name
+
+        self._create_directories()
+
+    def _create_directories(self) -> None:
+        self.source_code_directory_path.mkdir(parents=True, exist_ok=True)
+
+    @property
+    def source_code_directory_path(self) -> Path:
+        return self.output_directory_path / self.source_code_directory_name
