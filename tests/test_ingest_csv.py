324dcd0dfc4e7df20bfaed6f2441f62a75e0d123
Kristof Van Coillie <kristof.vancoillie@collibra.com>
2023-12-12 14:16:55 +0100

    Unit test update

--- tests/test_ingest_csv.py
+++ tests/test_ingest_csv.py
@@ -22,7 +22,7 @@ class TestIngestCSV(unittest.TestCase):
         self.csv_file_path = Path(self.filename)
         self.custom_lineage_config = CustomLineageConfig(
             application_name="unit tests csv",
-            output_directory="./tests/test_data/csv/ingested/",
+            output_directory="./test_data/csv/ingested/",
         )
 
     def test_validate_header(self):
@@ -300,17 +300,17 @@ class TestIngestCSV(unittest.TestCase):
 
     def test_ingest_csv_files(self):
         ingest_csv_files(
-            source_directory="./tests/test_data/csv",
+            source_directory="./test_data/csv",
             custom_lineage_config=self.custom_lineage_config,
         )
 
-        with open("./tests/test_data/csv/metadata.json") as input_file:
+        with open("./test_data/csv/metadata.json") as input_file:
             expected_metadata = json.load(input_file)
-        with open("./tests/test_data/csv/ingested/metadata.json") as input_file:
+        with open("./test_data/csv/ingested/metadata.json") as input_file:
             generated_metadata = json.load(input_file)
-        with open("./tests/test_data/csv/lineage_v3.json") as input_file:
+        with open("./test_data/csv/lineage_v3.json") as input_file:
             expected_lineage = json.load(input_file)
-        with open("./tests/test_data/csv/ingested/lineage.json") as input_file:
+        with open("./test_data/csv/ingested/lineage.json") as input_file:
             generated_lineage = json.load(input_file)
 
         # accodomate for random uuid in source code
@@ -324,7 +324,7 @@ class TestIngestCSV(unittest.TestCase):
 
         # cleanup
         shutil.rmtree(
-            "./tests/test_data/csv/ingested",
+            "./test_data/csv/ingested",
             ignore_errors=True,
         )
 
